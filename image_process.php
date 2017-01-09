0560933167d0a7641672ea7d71e1ddcb2f9e8879
pcsaini <premchandsaini779@gmail.com>
2017-01-09 23:38:21 +0530

    image upload complete

--- image_process.php
+++ image_process.php
@@ -4,6 +4,7 @@ if(isset($_FILES['image_upload_file'])){
     set_time_limit(0);
     $allowedImageType = array("image/png","image/jpg","image/jpeg");
     $path = "upload/";
+   // $thumb_path = 'upload/thumb/';
 
     if ($_FILES['image_upload_file']['error'] > 0){
         $output['error'] = "Error in File";
@@ -16,58 +17,9 @@ if(isset($_FILES['image_upload_file'])){
         $file = pathinfo($_FILES['image_upload_file']['name']);
         $fileType = $file["extension"];
         $fileNameNew = rand(333, 999).time().".$fileType";
-        $upload_image = $path.basename($fileNameNew);
-        $thumb_width = '50';
-        $thumb_height = '50';
-        $thumb = false;
-        print_r($thumb);
-        die();
 
-        if(move_uploaded_file($_FILES['image_upload_file']['tmp_name'],$upload_image))
-        {
-            //thumbnail creation
-            if($thumb == TRUE)
-            {
-                $thumbnail = $path.$fileNameNew;
-                list($width,$height) = getimagesize($upload_image);
-                $thumb_create = imagecreatetruecolor($thumb_width,$thumb_height);
-                switch($fileType){
-                    case 'jpg':
-                        $source = imagecreatefromjpeg($upload_image);
-                        break;
-                    case 'jpeg':
-                        $source = imagecreatefromjpeg($upload_image);
-                        break;
-
-                    case 'png':
-                        $source = imagecreatefrompng($upload_image);
-                        break;
-                    default:
-                        $source = imagecreatefromjpeg($upload_image);
-                }
-
-                imagecopyresized($thumb_create,$source,0,0,0,0,$thumb_width,$thumb_height,$width,$height);
-                switch($fileType){
-                    case 'jpg' || 'jpeg':
-                        imagejpeg($thumb_create,$thumbnail,100);
-                        break;
-                    case 'png':
-                        imagepng($thumb_create,$thumbnail,100);
-                        break;
-
-                    default:
-                        imagejpeg($thumb_create,$thumbnail,100);
-                }
-
-            }
-
-            return $fileNameNew;
-        }
-        else
-        {
-            return false;
-        }
-        //move_uploaded_file($temp_path,$path.$fileNameNew);
+        move_uploaded_file($temp_path,$path.$fileNameNew);
+        //createThumbs($path,$thumb_path,50,$fileNameNew);
         $output['image'] = $path.$fileNameNew;
         $output['status']=TRUE;
     }
